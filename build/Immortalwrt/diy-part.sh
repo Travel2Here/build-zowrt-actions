5e89f7f4ba5f275a4157e1d0cb0fc63c7e3e7971
Travel2Here <99887507+Travel2Here@users.noreply.github.com>
2024-11-18 16:10:56 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -76,21 +76,21 @@ sed -i 's/"实时流量监测"/"实时流量"/g' `egrep "实时流量监测" -rl
 sed -i 's/"TTYD 终端"/"TTYD"/g' `egrep "TTYD 终端" -rl ./`
 sed -i 's/"USB 打印服务器"/"打印服务"/g' `egrep "USB 打印服务器" -rl ./`
 sed -i 's/"Web 管理"/"Web管理"/g' `egrep "Web 管理" -rl ./`
-# sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
 sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 # sed -i 's/"设置向导"/"向导"/g' `egrep "设置向导" -rl ./`
+# sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
 
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
-cat >"$CLEAR_PATH" <<-EOF
-packages
-config.buildinfo
-feeds.buildinfo
-sha256sums
-version.buildinfo
-profiles.json
-openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-squashfs-rootfs.img.gz
-EOF
+# cat >"$CLEAR_PATH" <<-EOF
+# packages
+# config.buildinfo
+# feeds.buildinfo
+# sha256sums
+# version.buildinfo
+# profiles.json
+# openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-squashfs-rootfs.img.gz
+# EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
 cat >>$DELETE <<-EOF
